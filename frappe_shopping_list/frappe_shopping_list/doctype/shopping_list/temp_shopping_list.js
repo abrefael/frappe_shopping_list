e57de9def057ee13b143b689f3f15bc89733b96e
Alon Ben Refael <alonbr@proton.me>
2024-05-03 12:54:19 +0300

    Update temp_shopping_list.js

--- frappe_shopping_list/frappe_shopping_list/doctype/shopping_list/temp_shopping_list.js
+++ frappe_shopping_list/frappe_shopping_list/doctype/shopping_list/temp_shopping_list.js
@@ -12,10 +12,13 @@ function chngeColor(elem){
 	};
 	client.send();
 }
-    
+var flag = true;
+
 frappe.ui.form.on('shp', {
 	refresh: function(frm) {
         frm.fields_dict.list_chld.grid.wrapper.on('click', '.grid-row-check', function(event) {
+	    if (flag){
+		flag=false;
             if (event.target.checked == true){
                 var row = event.target.parentNode.parentNode.parentNode;
                 var lst = row.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.getAttribute('data-fieldname');
@@ -26,43 +29,40 @@ frappe.ui.form.on('shp', {
                 var unit;
                 var dest;
                 var src_lst = frm.doc.list_chld;
-                document.querySelectorAll('[data-doctype="Product"]').forEach((prod) => {
-                    if (prod.parentNode.parentNode.parentNode.parentNode.getAttribute('data-name') == idx){
-                            product = prod.getAttribute('data-value');
-                    }
-                });
-                document.querySelectorAll('[data-fieldname="quant"]').forEach((q) => {
-                    if (q.parentNode.parentNode.getAttribute('data-name') == idx){
-                            quant = q.lastChild.textContent;//('data-value');
-                    }
-                });
-                document.querySelectorAll('[data-fieldname="unit"]').forEach((u) => {
-                    if (u.parentNode.parentNode.getAttribute('data-name') == idx){
-                            unit = u.lastChild.textContent;//('data-value');
-                    }
-                });
                 if (lst == 'list_chld'){
                     dest = 'taken_chld';
                     console.log(dest);
                 }
-                else {
-                    src_lst = frm.doc.taken_chld;
-                    dest = 'list_chld';
-                }
-                var child = frm.add_child(dest);
-    			child.product = product;
-    			child.quant = quant;
-    			child.unit = unit;
-    			frm.refresh_field(dest);
-    			if (src_lst.length == 1){
-    				frm.set_value(lst, []);
-    			}
-    			else{
-    			    frm.get_field(lst).grid.grid_rows[row_num].remove();
-    			}
-    			frm.save();
+                document.querySelectorAll('[data-doctype="Product"]').forEach((prod) => {
+                    if (prod.parentNode.parentNode.parentNode.parentNode.getAttribute('data-name') == idx){
+	                    var child = frm.add_child(dest);
+			    child.product = prod.getAttribute('data-value');
+                    }
+                });
+                document.querySelectorAll('[data-fieldname="quant"]').forEach((q) => {
+                    if (q.parentNode.parentNode.getAttribute('data-name') == idx){
+                            child.quant = q.lastChild.textContent;//('data-value');
+                    }
+                });
+                document.querySelectorAll('[data-fieldname="unit"]').forEach((u) => {
+                    if (u.parentNode.parentNode.getAttribute('data-name') == idx){
+                            child.unit = u.lastChild.textContent;
+                    }
+                });
+		frm.refresh_field(dest);
+    		if (src_lst.length == 1){
+    			frm.set_value(lst, []);
+    		}
+    		else{
+    		    frm.get_field(lst).grid.grid_rows[row_num].remove();
+    		}
+    		if frm.is_dirty(){
+			frm.save();
+			flag=true;
+		}
             }
-        });
+	    }
+	});
     }
 });
 
